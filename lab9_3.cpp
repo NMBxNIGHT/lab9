b8043d46be9a8bb30049fcd7ced3b3e559580534
NMBxNIGHT <thiranat_kakanmee@cmu.ac.th>
2022-12-23 16:01:42 +0700

    9_3 sed leawwwww

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,43 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+
+#include <iostream>
+
+using namespace std;
+int main(){
+    int age;
+    long long int bounty;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age<=25){
+        float tall;
+        cout << "Enter your height: ";
+        cin >> tall ;
+        if(tall <100){
+            character = "Chopper";
+        }else if(tall <180){
+            character ="Usopp";
+        }else{
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+            if(bounty>1.1e9){
+                character = "Zoro";
+            }else{
+                character = "Sanji";
+            }
+        }
+    }else if(age<=60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 5e8){
+            character = "Jinbe";
+        }else{
+            character ="Franky";
+        }
+    }else{
+        character ="Brook";
+    }
+
+    cout << "Your character = " << character;
+
+    return 0;
+}
